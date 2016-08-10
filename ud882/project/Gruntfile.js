f9c8eaf08bfb4f4241f5576e884f4aa8e6bdb22b
Chris <cphurley82@gmail.com>
2016-08-10 16:56:26 -0700

    merged in RI Project Part 1 Solution *corrected*

--- ud882/project/Gruntfile.js
+++ ud882/project/Gruntfile.js
@@ -1,5 +1,5 @@
 /*
- After you have changed the settings at "Your code goes here",
+ After you have changed any settings for the responsive_images task,
  run this with one of these options:
   "grunt" alone creates a new, completed images directory
   "grunt clean" removes the images directory
@@ -14,19 +14,11 @@ module.exports = function(grunt) {
         options: {
           engine: 'gm',
           sizes: [{
-            /*
-            Change these:
-            */
             width: 1600,
-            suffix: "_large_2x",
+            suffix: '_large_2x',
             quality: 30
           }]
         },
-
-        /*
-        You don't need to change this part if you don't change
-        the directory structure.
-        */
         files: [{
           expand: true,
           src: ['*.{gif,jpg,png}'],
@@ -57,16 +49,19 @@ module.exports = function(grunt) {
       dev: {
         files: [{
           expand: true,
-          src: 'images_src/fixed/*.{gif,jpg,png}',
-          dest: 'images/'
+          src: ['images_src/fixed/*.{gif,jpg,png}'],
+          dest: 'images/',
+          flatten: true,
         }]
       },
     },
+
   });
-  
+
   grunt.loadNpmTasks('grunt-responsive-images');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-mkdir');
   grunt.registerTask('default', ['clean', 'mkdir', 'copy', 'responsive_images']);
+
 };
